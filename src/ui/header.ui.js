03516f85988cc1fd70c8817ece2d4dba2d32c602
Lorellana <lorellana.ordonez@gmail.com>
2022-05-25 19:00:33 +0200

    fix(ui): add solution to bug in header menu responsive

--- src/ui/header.ui.js
+++ src/ui/header.ui.js
@@ -1,110 +1,25 @@
-import { css, html, LitElement } from "lit-element";
+import { html, LitElement } from "lit-element";
 
 export class HeaderUI extends LitElement {
   static get properties() {
     return {
       link1: { type: String },
       link2: { type: String },
+      burguer: {type: Boolean}
     };
   }
   constructor() {
     super();
     this.link1 = "Namasté";
     this.link2 = "Yoga session";
-  }
-  static get styles() {
-    return css`
-      header {
-        width: 100%;
-        height: 100%;
-        position: fixed;
-        top: 0;
-        left: 0;
-        z-index: 1;
-      }
-      header nav {
-        display: flex;
-        flex-flow: row wrap;
-        align-items: center;
-        width: 90%;
-        height: 3rem;
-        margin: 20px auto;
-      }
-      header nav ul {
-        font-size: var(--size-links-pages);
-        color: var(--color-primary);
-        box-shadow: 10px 5px 5px var(--color-primary);
-        display: flex;
-        flex-flow: row-reverse;
-        gap: 1rem;
-        align-items: center;
-        width: 320px;
-        cursor: pointer;
-        position: relative;
-        list-style-type: none;
-      }
-      a {
-        text-decoration: none;
-      }
-      a:hover {
-        color: var(--color-secondary-links);
-      }
-
-      /*header-mobile navigation*/
-
-      .burguer {
-        cursor: pointer;
-        display: none;
-      }
-      .burguer div {
-        margin: 10px;
-        padding: 5px 5px 1px 1px;
-      }
-      .burguer .bar {
-        background-color: var(--color-primary);
-        width: 40px;
-        height: 3px;
-        border-radius: 5px;
-      }
-
-      @media (max-width: 400px) {
-        .burguer {
-          display: block;
-        }
-        header nav ul {
-          flex-flow: column wrap;
-          position: absolute;
-          padding: 0.2em 0.2em;
-          background: var(--color-primary-light);
-          transition: transform 0.3s ease-in-out;
-          border-radius: 5em;
-          border: 2px solid var(--color-secondary-links);
-          box-shadow: none;
-          transform: translateX(150%);
-        }
-        header nav ul li {
-          font-size: 1.5rem;
-          color: var(--color-primary);
-        }
-      }
-      .open {
-        transform: translateX(0%);
-      }
-    `;
+    this.burguer = false
   }
 
-  firstUpdated() {
-    const burguer = this.querySelector(".burguer");
+  
+  __toggle() {
     const menu = this.querySelector("header nav ul");
-    burguer.addEventListener("click", function () {
-      menu.classList.toggle("open");
-    });
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this.firstUpdated();
-  }
+    menu.classList.toggle("open");
+    }
 
   createRenderRoot() {
     return this;
@@ -114,12 +29,12 @@ export class HeaderUI extends LitElement {
     return html`
       <header>
         <nav>
-          <div class="burguer">
+          <div class="burguer" @click=${this.__toggle}>
             <div class="bar"></div>
             <div class="bar"></div>
             <div class="bar"></div>
           </div>
-          <ul>
+          <ul class="links">
             <li><a href="/poses">${this.link2}</a></li>
             <li><a href="/">${this.link1}</a></li>
           </ul>
